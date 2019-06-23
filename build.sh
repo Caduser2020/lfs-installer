728187c1d69bbe83097490b5274252a75aff54ff
Caduser2020 <51916507+Caduser2020@users.noreply.github.com>
2019-06-23 17:05:27 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Enter previous password set
 whoami 
-sleep 5
 cat > ~/.bash_profile << 'EOF' 
 exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash 
 EOF
@@ -17,7 +16,6 @@ export LFS LC_ALL LFS_TGT PATH
 EOF
 source ~/.bash_profile
 cd /mnt/lfs/sources
-sleep 5
 tar xvf binutils-2.32.tar.xz
 cd binutils-2.32
  mkdir -v build; cd build
@@ -36,15 +34,15 @@ tar -xf ../gmp-6.1.2.tar.xz
 mv -v gmp-6.1.2 gmp
 tar -xf ../mpc-1.1.0.tar.gz
 mv -v mpc-1.1.0 mpc
-for file in gcc/config/{linux,i386/linux{,64}}.h \
-do \
+for file in gcc/config/{linux,i386/linux{,64}}.h 
+do 
  cp -uv $file{,.orig} \
  sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
  -e 's@/usr@/tools@g' $file.orig > $file \
- echo ' \
-#undef STANDARD_STARTFILE_PREFIX_1 \
-#undef STANDARD_STARTFILE_PREFIX_2 \
-#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/" \
+ echo ' 
+#undef STANDARD_STARTFILE_PREFIX_1 
+#undef STANDARD_STARTFILE_PREFIX_2 
+#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/" 
 #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file \
  touch $file.orig \
 done

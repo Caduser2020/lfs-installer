542088ba8e49a8c8be4f6619c706af7a577bd866
Caduser2020 <51916507+Caduser2020@users.noreply.github.com>
2019-06-23 09:45:19 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 # Enter previous password set
 whoami 
 sleep 5
-cat > ~/.bash_profile << 'EOF' \
-exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash \
+cat > ~/.bash_profile << 'EOF' 
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash 
 EOF
 
-cat > ~/.bashrc << 'EOF' \
-set +h \
-umask 022 \
-LFS=/mnt/lfs \
-LC_ALL=POSIX \
-LFS_TGT=$(uname -m)-lfs-linux-gnu \
-PATH=/tools/bin:/bin:/usr/bin \
-export LFS LC_ALL LFS_TGT PATH \
+cat > ~/.bashrc << 'EOF' 
+set +h 
+umask 022 
+LFS=/mnt/lfs 
+LC_ALL=POSIX 
+LFS_TGT=$(uname -m)-lfs-linux-gnu 
+PATH=/tools/bin:/bin:/usr/bin 
+export LFS LC_ALL LFS_TGT PATH 
 EOF
 source ~/.bash_profile
 cd /mnt/lfs/sources

8445c067aa7435050ce40d9908b81568ca1ad4c0
Cad User 2020 <iposton73@outlook.com>
2019-07-03 11:25:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@
 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+sudo yum -y update
 sudo yum -y install bison byacc gcc-c++ patch texinfo
 cat > version-check.sh << "EOF"
 #!/bin/bash

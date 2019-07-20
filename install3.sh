564caa749ef715f94a671f588834e2fbdfacf52c
Cad User 2020 <iposton73@outlook.com>
2019-07-19 20:02:53 -0500

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -58,3 +58,40 @@ read -p "Press [Enter] key to resume..."
 cd /mnt/lfs/sources
 rm -Rf file-5.36
 
+# Readline-8.0 || A set of libraries that offers command-line editing and history capabilitiese || 0.1 SBUs
+tar xvf readline-8.0.tar.gz
+cd readline-8.0
+sed -i '/MV.*old/d' Makefile.in
+sed -i '/{OLDSUFF}/c:' support/shlib-install
+./configure --prefix=/usr \
+--disable-static \
+--docdir=/usr/share/doc/readline-8.0
+read -p "Press [Enter] key to resume..."
+make SHLIB_LIBS="-L/tools/lib -lncursesw"
+read -p "Press [Enter] key to resume..."
+make SHLIB_LIBS="-L/tools/lib -lncursesw" install
+read -p "Press [Enter] key to resume..."
+mv -v /usr/lib/lib{readline,history}.so.* /lib
+chmod -v u+w /lib/lib{readline,history}.so.*
+ln -sfv ../../lib/$(readlink /usr/lib/libreadline.so) /usr/lib/libreadline.so
+ln -sfv ../../lib/$(readlink /usr/lib/libhistory.so ) /usr/lib/libhistory.so
+install -v -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/readline-8.0
+read -p "Press [Enter] key to resume..."
+cd /mnt/lfs/sources
+rm -Rf readline-8.0
+
+# M4-1.4.18 || Copies the given files while expanding the macros that they contain || 0.4 SBUs
+tar xvf m4-1.4.18.tar.xz
+cd m4-1.4.18
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+./configure --prefix=/usr 
+read -p "Press [Enter] key to resume..."
+make
+read -p "Press [Enter] key to resume..."
+make check
+read -p "Press [Enter] key to resume..."
+make install
+read -p "Press [Enter] key to resume..."
+cd /mnt/lfs/sources
+rm -Rf m4-1.4.18

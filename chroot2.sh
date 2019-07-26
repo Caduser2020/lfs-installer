0d24651ecb19c83efdb6e727ff883a165d9c9b01
Cad User 2020 <iposton73@outlook.com>
2019-07-26 11:08:17 -0500

    Fix chroot error

---
+++ chroot2.sh
@@ -0,0 +1,87 @@
+#!/bin/bash  
+#=================================================================================== 
+# 
+# Set up Chroot for Linux From Scratch 8.4 on a Red Hat based distribution of linux, such as Fedora, CentOS, or RHEL. 
+# Copyright (C) 2019 
+ 
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU Affero General Public License as published 
+# by the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version. 
+ 
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Affero General Public License for more details. 
+ 
+# You should have received a copy of the GNU Affero General Public License 
+# along with this program.  If not, see <https://www.gnu.org/licenses/> 
+# 
+#===================================================================================
+
+mkdir -pv /{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}
+mkdir -pv /{media/{floppy,cdrom},sbin,srv,var}
+install -dv -m 0750 /root
+install -dv -m 1777 /tmp /var/tmp
+mkdir -pv /usr/{,local/}{bin,include,lib,sbin,src}
+mkdir -pv /usr/{,local/}share/{color,dict,doc,info,locale,man}
+mkdir -v /usr/{,local/}share/{misc,terminfo,zoneinfo}
+mkdir -v /usr/libexec
+mkdir -pv /usr/{,local/}share/man/man{1..8}
+case $(uname -m) in
+x86_64) mkdir -v /lib64 ;;
+esac
+mkdir -v /var/{log,mail,spool}
+ln -sv /run /var/run
+ln -sv /run/lock /var/lock
+mkdir -pv /var/{opt,cache,lib/{color,misc,locate},local}
+read -p "Press [Enter] key to resume..."
+
+ln -sv /tools/bin/{bash,cat,chmod,dd,echo,ln,mkdir,pwd,rm,stty,touch} /bin
+ln -sv /tools/bin/{env,install,perl,printf} /usr/bin
+ln -sv /tools/lib/libgcc_s.so{,.1} /usr/lib
+ln -sv /tools/lib/libstdc++.{a,so{,.6}} /usr/lib
+install -vdm755 /usr/lib/pkgconfig
+ln -sv bash /bin/sh
+read -p "Press [Enter] key to resume..."
+
+ln -sv /proc/self/mounts /etc/mtab
+cat > /etc/passwd << "EOF"
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/dev/null:/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
+nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
+EOF
+read -p "Press [Enter] key to resume..."
+
+cat > /etc/group << "EOF"
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+usb:x:14:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+input:x:24:
+mail:x:34:
+kvm:x:61:
+wheel:x:97:
+nogroup:x:99:
+users:x:999:
+EOF
+read -p "Press [Enter] key to resume..."
+
+exec /tools/bin/bash --login +h
+
